0d5965b8f7eeba5fbb4beac0d53ff1abda1f46e7
ChachisV <javanmot@gmail.com>
2022-05-17 01:03:53 -0500

    hicimos la funcion de descuentos con if el if usando el error first y arrays

--- descuentos/descuentos.js
+++ descuentos/descuentos.js
@@ -49,6 +49,53 @@
 
 // Cupones de descuento con arrays y switch
 
+
+// const coupons = [
+//     "ChachisV", 
+//     "SuperSaiyan", 
+//     "SuperChachisV",
+// ];
+
+// function calcularPrecioConDescuento(precio, descuento){
+//     const porcentajePrecioConDescuento = 100 - descuento;
+//     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
+
+//     return precioConDescuento;
+// }
+
+
+// function onClickButtonPriceDiscount() {
+//     const inputPrice = document.getElementById("inputPrice");
+//     const priceValue = inputPrice.value;
+
+//     const inputCoupon = document.getElementById("inputCoupon");
+//     const couponValue = inputCoupon.value;
+
+//     let descuento;
+
+//     switch(couponValue){
+//         case coupons[0]: // "ChachisV"
+//             descuento = 15;
+//         break;
+//         case coupons[1]: // "SuperSaiyan"
+//             descuento = 20;
+//         break;
+//         case coupons[2]: // "SuperChachisV"
+//             descuento = 35;
+//         break;
+
+//     }
+
+
+//     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+//     const resultP = document.getElementById("resultP");
+//     resultP.innerHTML = "El precio con descuento es $" + precioConDescuento ;
+// }
+
+
+// legibilidad, error first y muerte al switch
+
 const coupons = [
     "ChachisV", 
     "SuperSaiyan", 
@@ -72,19 +119,18 @@ function onClickButtonPriceDiscount() {
 
     let descuento;
 
-    switch(couponValue){
-        case coupons[0]: // "ChachisV"
-            descuento = 15;
-        break;
-        case coupons[1]: // "SuperSaiyan"
-            descuento = 20;
-        break;
-        case coupons[2]: // "SuperChachisV"
-            descuento = 35;
-        break;
-
+    if (!coupons.includes(couponValue)){
+        alert("El cupón " + couponValue + " no existe");
     }
-
+    else if (couponValue === "ChachisV") {
+        descuento = 15 
+    }
+    else if (couponValue === "SuperSaiyan") {
+        descuento = 25 
+    }
+    else if (couponValue === "SuperChachisV") {
+        descuento = 35 
+    }
 
     const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
