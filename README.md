8f4583b8d48638e7a545edf7ac55ef25edfea8c2
ChachisV <javanmot@gmail.com>
2022-05-16 23:45:17 -0500

    empezamos el nuevo taller de descuentos , creamos el descuento.html y el descuentos.js ademas actualizamos el readme

--- README.md
+++ README.md
@@ -4,7 +4,15 @@
 
 ## Taller #1 Figuras Geometricas
 
-- Primer Paso: definir las formulas
+- Primer Paso: definir las fórmulas
+- Segundo Paso: implemetar Fórmulas en Javascript
+- Tercer Paso : crear funciones
+- Cuarto paso: integrar JS con HTML
+
+## Taller #2 Porcentajes y descuentos
+
+- Primer Paso: definir las fórmulas
 - Segundo Paso: implemetar Formulas en Javascript
 - Tercer Paso : crear funciones
 - Cuarto paso: integrar JS con HTML
+
---
+++ descuentos.js
@@ -0,0 +1,32 @@
+// const precioOriginal = 120;
+// const descuento = 18;
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento)/100;
+
+    return precioConDescuento;
+}
+
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("resultP");
+    resultP.innerHTML = "El precio con descuento es $" + precioConDescuento ;
+}
+
+
+// console.log({
+//     precioOriginal, 
+//     descuento, 
+//     porcentajePrecioConDescuento, 
+//     precioConDescuento
+// })
